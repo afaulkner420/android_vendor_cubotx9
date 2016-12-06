55030d642934f017b592353a55afeb703ea1aa4f
afaulkner420 <afaulkner420@gmail.com>
2016-12-06 21:42:50 +0000

    First Commit

---
+++ cubotx9-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Cubot/cubotx9/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Cubot/cubotx9/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Cubot/cubotx9/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Cubot/cubotx9/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Cubot/cubotx9/proprietary/vendor,system/vendor)
